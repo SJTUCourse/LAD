708ebddc0b20e0be3d78c9f190e3ec21ce728a4c
looklucas <27678380@qq.com>
2018-10-18 17:00:15 +0800

    recover 1

---
+++ project 1/AI_InstantAI/ai_instant.h
@@ -0,0 +1,66 @@
+#ifndef AI_INSTANT_H
+#define AI_INSTANT_H
+
+#include <QtWidgets/QDialog>
+#include <QtCore/QtCore>
+#include <QTimer>
+#include "ui_ai_instant.h"
+#include "../common/simplegraph.h"
+#include "configuredialog.h"
+#include <QtCore/QCoreApplication>
+#include <QFile>
+#include <QString>
+#include <QTextStream>
+#include "fft_dialog.h"
+
+const QString FILE_PATH_0(".\\channel_0.txt");
+const QString FILE_PATH_1(".\\channel_1.txt");
+const QString FILE_PATH_2(".\\channel_2.txt");
+const QString FILE_PATH_3(".\\channel_3.txt");
+const QString FILE_PATH_4(".\\channel_4.txt");
+const QString FILE_PATH_5(".\\channel_5.txt");
+const QString FILE_PATH_6(".\\channel_6.txt");
+const QString FILE_PATH_7(".\\channel_7.txt");
+
+class AI_Instant : public QDialog
+{
+	Q_OBJECT
+
+
+public:
+    AI_Instant(QWidget *parent = nullptr, Qt::WindowFlags flags = nullptr);
+    ~AI_Instant();
+	void Initialize();
+	void ConfigureDevice();
+	void ConfigureGraph();
+	void CheckError(ErrorCode errorCode);
+	void RefreshList();
+	void SetConfigureDialog(ConfigureDialog *dialog){this->configDialog = dialog;}
+	void SetConfigureParameter(ConfigureParameter value){this->configure = value;}
+
+
+private:
+	Ui::AI_InstantFrame ui;
+	SimpleGraph *graph;
+	ConfigureDialog *configDialog;
+	ConfigureParameter configure;
+    fft_dialog *fre_graph;
+	InstantAiCtrl *instantAiCtrl;
+	double scaledData[16];
+    QTimer *timer;
+    void mousePressEvent(QMouseEvent *event);
+
+private slots:
+    void TimerTicked();
+	void SliderValueChanged(int value);
+    void SliderXScaleChanged(int value);
+    void SliderYScaleChanged(int value);
+	void ButtonStartClicked();
+	void ButtonConfigureClicked();
+	void ButttonPauseClicked();
+	void ButtonStopClicked();
+    void ButtonFFTClicked();
+    void CmbIndexChanged(int);
+};
+
+#endif // AI_INSTANT_H
---
+++ project 1/AI_InstantAI/fft_dialog.h
@@ -0,0 +1,50 @@
+#ifndef FFT_DIALOG_H
+#define FFT_DIALOG_H
+
+#include <QDialog>
+#include <QtCore/QtCore>
+#include "../common/simplegraph.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+namespace Ui {
+class fft_dialog;
+}
+
+class fft_dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit fft_dialog(QWidget *parent = nullptr);
+    ~fft_dialog();
+    double fs = 100;
+    void update();
+    int channel = 0;
+
+    void IFFT (double xreal [], double ximag [], int n);
+    void FFT(double xreal [], double ximag [], int n);
+    void bitrp (double xreal [], double ximag [], int n);
+    inline void swap (double &a, double &b);
+    double xreal[1024],ximag[1024];
+
+private:
+    SimpleGraph *graph_fre;
+    SimpleGraph *graph_time;
+    Ui::fft_dialog *ui;
+    int N =1024;
+    int order_filter_min;
+    int order_filter_max;
+    double size_change_form_origin;
+
+private slots:
+    void ButtonLowPassClicked();
+    void ButtonHighPassClicked();
+    void FFT_show ();
+    void ButttonSizeChangeClicked();
+    void SLideFreChange(int);
+    void SLideSizeChange(int);
+};
+
+#endif // FFT_DIALOG_H
